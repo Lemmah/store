013c2a5c32a9c903e7c89d2514837f263373ffea
Lemmah <jnlemayian@gmail.com>
2026-06-29 15:33:42 +0300

    test: fix failing tests

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -2,10 +2,20 @@ require "test_helper"
 
 class ProductMailerTest < ActionMailer::TestCase
   test "in_stock" do
-    mail = ProductMailer.in_stock
-    assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    product = products(:tshirt)
+    subscriber = subscribers(:lemmah)
+
+    mail = ProductMailer.with(
+      product: product,
+      subscriber: subscriber
+    ).in_stock
+
+    assert_equal "Product back in stock!", mail.subject
+    assert_equal [ "test@lemmah.test" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "back in stock", mail.subject
+    assert_match "Good news!", mail.body.encoded
+    assert_match "#{product.name}", mail.body.encoded
+    assert_match "Unsubscribe", mail.body.encoded
   end
 end
