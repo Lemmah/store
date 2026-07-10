498a0f5995ec582e3807f641cd78fb3abf519875
Lemmah <jnlemayian@gmail.com>
2026-07-11 00:47:30 +0300

    test: fix broken user/mailer tests

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,10 +2,12 @@ require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
   test "email_confirmation" do
-    mail = UserMailer.email_confirmation
+    user = users(:one)
+    user.update_column(:unconfirmed_email, "new-email@example.com")
+    mail = UserMailer.with(user: user).email_confirmation
     assert_equal "Email confirmation", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    assert_equal [ user.unconfirmed_email ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Confirm your email", mail.body.encoded
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require "rails/test_help"
 require_relative "test_helpers/session_test_helper"
 
 module ActiveSupport
+  include SessionTestHelper
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
