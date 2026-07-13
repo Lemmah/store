41929fee007ad887dd1efb1ca8c0385cbed5ecc9
Lemmah <jnlemayian@gmail.com>
2026-07-14 01:34:16 +0300

    test: user can edit a wishlist

--- test/controllers/wishlists_controller_test.rb
+++ test/controllers/wishlists_controller_test.rb
@@ -25,4 +25,21 @@ class WishlistsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to wishlist_path(wishlist)
     assert_equal "Your wishlist was created successfully.", flash[:notice]
   end
+
+  test "user can edit a wishlist" do
+    user = users(:one)
+    sign_in_as user
+    wishlist = user.wishlists.first
+    previous_wishlist_name = wishlist.name
+    put wishlist_path(wishlist), params: {
+      wishlist: {
+        name: "Updated Wishlist Test"
+      }
+    }
+    wishlist.reload
+    refute_equal previous_wishlist_name, wishlist.name
+    assert_equal "Updated Wishlist Test", wishlist.name
+    assert_redirected_to wishlist_path(wishlist)
+    assert_equal "Your wishlist has been updated successfully.", flash[:notice]
+  end
 end
