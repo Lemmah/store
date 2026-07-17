c3b8d0c439aba2d51ac94cac8c9df80a27b17e76
Lemmah <jnlemayian@gmail.com>
2026-07-18 02:13:02 +0300

    refactor: mv filter_by logic from controller to model

--- app/controllers/store/wishlists_controller.rb
+++ app/controllers/store/wishlists_controller.rb
@@ -1,10 +1,6 @@
 class Store::WishlistsController < Store::BaseController
   def index
-    @wishlists = Wishlist.includes(:user).all
-    @wishlists = @wishlists.where(user_id: params[:user_id]) if params[:user_id].present?
-    @wishlists = @wishlists.includes(:wishlist_products).where(
-      wishlist_products: { product_id: params[:product_id] }
-    ) if params[:product_id].present?
+    @wishlists = Wishlist.filter_by(params)
   end
 
   def show
