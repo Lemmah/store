078646d4a6f58b4354fd94d6c3cd4aad3b054f4a
Lemmah <jnlemayian@gmail.com>
2026-07-20 21:33:32 +0300

    test: cover subscriber.filter_by behavior

--- test/models/subscriber_test.rb
+++ test/models/subscriber_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class SubscriberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "filter_by with no filters returns all subscribers" do
+    assert_equal subscribers.to_set, Subscriber.filter_by({}).to_set
+  end
+
+  test "filter_by orders by created_at in descending" do
+    expected = [
+      subscribers(:lemmah),
+      subscribers(:bern),
+      subscribers(:joe)
+    ]
+    assert_equal expected, Subscriber.filter_by({})
+  end
+
+  test "filter with product_id returns only subscribers for that product" do
+    product = products(:short)
+    expected = [ subscribers(:joe) ]
+    assert_equal expected, Subscriber.filter_by({ product_id: product.id })
+  end
 end
