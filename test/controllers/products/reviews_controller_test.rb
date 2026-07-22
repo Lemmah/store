6cdf0c6672bf5f4c9f60c160484a5c1ca481fb8d
Lemmah <jnlemayian@gmail.com>
2026-07-22 16:39:17 +0300

    test: cover product reviews index

--- test/controllers/products/reviews_controller_test.rb
+++ test/controllers/products/reviews_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class Products::ReviewsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get products_reviews_create_url
+  test "should list reviews for a product on index" do
+    sign_in_as users(:one)
+    get product_reviews_path(products(:tshirt))
     assert_response :success
+    assert_dom "h1", text: "1 review for T-Shirt"
   end
 end
