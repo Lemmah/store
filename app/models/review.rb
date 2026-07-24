147707ebadfc0040d34d181a9ec5ac7b8bd060dd
Lemmah <jnlemayian@gmail.com>
2026-07-24 20:55:08 +0300

    refactor: rename calculate to recalculate

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,12 +4,12 @@ class Review < ApplicationRecord
 
   scope :newest_first, -> { order(created_at: :desc) }
 
-  after_commit :calculate_average_star_rating,
+  after_commit :recalculate_average_star_rating,
     on: %i[ create update destroy ]
 
   private
 
-  def calculate_average_star_rating
+  def recalculate_average_star_rating
     return if transaction_include_any_action?([ :update ]) &&
       !saved_change_to_star_rating?
 
