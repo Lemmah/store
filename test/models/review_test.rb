8a224c25cd25845db0ecc66e03ea6a27cf4e7098
Lemmah <jnlemayian@gmail.com>
2026-07-26 02:49:27 +0300

    test: cover product star ratings sum behavior

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @product = products(:one)
+    @user = users(:one)
+  end
+
+  test "creating a review increases product star ratings sum" do
+    assert_difference -> { @product.reload.star_ratings_sum }, 4 do
+      @product.reviews.create!(
+        user_id: @user.id,
+        star_rating: 4
+      )
+    end
+  end
+
+  test "destroying a review decreases product star ratings sum" do
+    review = @product.reviews.new(
+      user_id: users(:one).id,
+      star_rating: 5
+    )
+    review.save!
+    assert_difference -> { @product.reload.star_ratings_sum }, -5 do
+      review.destroy!
+    end
+  end
+
+  test "decreasing a review rating decreases product star ratings sum" do
+    review = @product.reviews.new(
+      user_id: @user.id,
+      star_rating: 5
+    )
+    review.save!
+    assert_difference -> { @product.reload.star_ratings_sum }, -2 do
+      review.update!(star_rating: 3)
+    end
+  end
+
+  test "increasing a review rating increases product star ratings sum" do
+    review = @product.reviews.new(
+      user_id: @user.id,
+      star_rating: 3
+    )
+    review.save!
+    assert_difference -> { @product.reload.star_ratings_sum }, 2 do
+      review.update(star_rating: 5)
+    end
+  end
 end
